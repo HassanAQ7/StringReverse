0402a3258999167b227951096d52748751be27ef
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-06-15 23:20:21 -0700

    implement reverseSentence method

--- src/reverse/StringReverser.java
+++ src/reverse/StringReverser.java
@@ -11,5 +11,20 @@ public class StringReverser {
         return buffer.reverse().toString();
     }
 
-    
+    public static String reverseSentence(String sentence) {
+        if (sentence.isBlank() && sentence.isEmpty()) {
+            return sentence;
+        }
+
+        String[] words = sentence.split(" ");
+        StringBuffer reversedSentence = new StringBuffer();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversedSentence.append(words[i]);
+            if (i != 0) {
+                reversedSentence.append(" ");
+            }
+        }
+        return reversedSentence.toString();
+    }
+
 }
